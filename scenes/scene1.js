8dc9ffe186324f22e6af12a48cc345ffc48ab30e
Alex Wassel <awassel18@gmail.com>
2020-06-06 16:23:43 -0700

    scene1

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -4,12 +4,10 @@ class scene1 extends Phaser.Scene{
     constructor() {
         super("bootGame");
       }
-    
       preload(){
         this.load.image("background", STATIC_URL + "sprites/office_room_bg.png");
         this.load.audio("music", [STATIC_URL + "audio/calm_theme.mp3"]);
       }
-    
       create() {
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
