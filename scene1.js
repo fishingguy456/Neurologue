ef51a6cd089bbb3ddc49a8ad1c6da14545cee63e
Kevin Cui JY <kevincui1230@gmail.com>
2020-06-06 05:43:44 -0500

    Add static urls

--- scene1.js
+++ scene1.js
@@ -1,11 +1,13 @@
+const STATIC_URL = "http://localhost:9000/assets/"
+
 class scene1 extends Phaser.Scene{
     constructor() {
         super("bootGame");
       }
     
       preload(){
-        this.load.image("background", "https://raw.githubusercontent.com/ansimuz/getting-started-with-phaser/master/part%203%20Game%20Objects%20-%20Images/assets/images/background.png");
-        this.load.audio("music", ["https://raw.githubusercontent.com/fishingguy456/HTNEphaser/master/assets/audio/calm_theme.mp3"]);
+        this.load.image("background", STATIC_URL + "sprites/office_room_bg.png");
+        this.load.audio("music", [STATIC_URL + "audio/calm_theme.mp3"]);
       }
     
       create() {
