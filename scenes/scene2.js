7eba454b1676c6b68287f47171349e3992ae53a9
Kevin Cui JY <kevincui1230@gmail.com>
2020-06-06 10:19:42 -0500

    Add data reader

--- scenes/scene2.js
+++ scenes/scene2.js
@@ -1,20 +1,79 @@
+var goto = 1;
+var clicked = 0;
+var node = null;
+
+
 class scene2 extends Phaser.Scene {
   constructor() {
     super("scene_2");
-  }
-  create() {
+  };
+
+  preload() {
     this.music1 = this.sound.add("calm_theme");
     this.background = this.add.image(0, 0, "office");
     this.background.setOrigin(0, 0);
-    this.box = this.add.image(22, 578, "text_box");
-    this.box.setOrigin(0, 0);
     this.music1.play();
   }
 
+  create() {
+    this.box = this.add.image(22, 578, "text_box");
+    this.box.setOrigin(0, 0);
+    this.person = this.add.image(50, 178, "guy_smile");
+    this.person.setOrigin(0, 0);
+    this.person.visible = false;
+    var style = {font: "20px", wordWrap: {width: 923}};
+    this.textInfo = this.add.text(50, 600,"How brave of you, mocking the face of controvery! Do you imply that the workings of the mind are at the effect of our surroundings? Or do you question the true reality of our dreams? This, I must know more.", style);
+    this.option1 = this.add.text(100, 680, "[OK]", style);
+    this.option2 = this.add.text(700, 680, "[OK]", style);
+    var nodes = this.cache.json.get('data_part1');
+    node = nodes[goto-1]
+    if (node["image"] === null)
+      this.person.visible = false;
+    else {
+      this.person.visible = true
+      this.person.setTexture(node["image"])
+    }
+    this.textInfo.setText(node["text"])
+    this.option1.setText('[' + Object.keys(node['options'])[0] + ']')
+    if (Object.keys(node['options']).length > 1) {
+      this.option2.visible = true;
+      this.option2.setText('[' + Object.keys(node['options'])[1] + ']')
+    } 
+    else
+      this.option2.visible = false
+    this.option1.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.option1.width, this.option1.height), Phaser.Geom.Rectangle.Contains);
+    this.option2.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.option2.width, this.option2.height), Phaser.Geom.Rectangle.Contains);
+  }
+
   update() {
-    this.guySmile = this.add.image(22, 178, "guy_smile");
-    this.guySmile.setOrigin(0, 0);
-    var style = {font: "20px", wordWrap: {width: 950}}
-    this.textInfo = this.add.text(27,583,"", style);
+    this.option1.on('pointerdown', function() {
+      clicked = 1;
+    })
+    this.option2.on('pointerdown', function() {
+      if (this.visible)
+        clicked = 2;
+    })
+    if (clicked > 0) {
+      var nodes = this.cache.json.get('data_part1');
+      goto = node['options'][Object.keys(node['options'])[clicked-1]]['dest'][0];
+      node = nodes[goto-1]
+      if (node["image"] === null)
+        this.person.visible = false;
+      else {
+        this.person.visible = true
+        this.person.setTexture(node["image"])
+      }
+      this.textInfo.setText(node["text"])
+      this.option1.setText('[' + Object.keys(node['options'])[0] + ']')
+      if (Object.keys(node['options']).length > 1) {
+        this.option2.visible = true;
+        this.option2.setText('[' + Object.keys(node['options'])[1] + ']')
+      } 
+      else
+        this.option2.visible = false
+      clicked = 0;
+
+    }
+
   }
 }
