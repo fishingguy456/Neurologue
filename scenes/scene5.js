ee05d44f3beca69d2287b43983efcfd82b95bdb2
kevin qu <qukevin99@gmail.com>
2020-06-07 13:36:49 -0400

    end text

--- scenes/scene5.js
+++ scenes/scene5.js
@@ -17,12 +17,21 @@ class scene5 extends Phaser.Scene {
         repeat: -1,
       });
       this.person.play("laugh_anim");
+      var style = {font: "48px", wordWrap: {width: 923}};
+      this.end = this.add.text(512, 100,"YOU UNLOCKED THE BAD ENDING", style);
+      this.end.x = 512-this.end.width/2;
     }if(goodEnding){
       this.background = this.add.image(0, 0, "good_ending");
       this.background.setOrigin(0, 0);
+      var style = {font: "48px", wordWrap: {width: 923}};
+      this.end = this.add.text(512, 100,"YOU UNLOCKED THE GOOD ENDING", style);
+      this.end.x = 512-this.end.width/2;
     }if(trueEnding){
       this.background = this.add.image(0, 0, "true_ending");
       this.background.setOrigin(0, 0);
+      var style = {font: "48px", wordWrap: {width: 923}};
+      this.end = this.add.text(512, 100,"YOU UNLOCKED THE TRUE ENDING", style);
+      this.end.x = 512-this.end.width/2;
     }
   }
 }
